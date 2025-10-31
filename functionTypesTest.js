119147d02ba51e59c34d90bbea359eefbb114b34
dpalexandru <alexpoian91@gmail.com>
2025-10-31 09:02:04 +0530

    snack1 - function types test

---
+++ functionTypesTest.js
@@ -0,0 +1,28 @@
+// Crea una funzione che somma due numeri.
+//Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+//Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+//Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+// Creo una funzione dichiarativa che restituisce la somma di due numeri 
+function somma(a, b) {
+  let sum = a + b
+  console.log(sum)
+}
+
+somma(2, 9);
+
+// assegno una funzione anonima a una variabile 
+const sommaInUnaVariabile = function (a, b) {
+  let sum = a + b
+  console.log(sum)
+}
+
+sommaInUnaVariabile(3, 4);
+
+//scrivo somma con arrow function 
+const sommaArrow = (a, b) => {
+  let sum = a + b
+  console.log(sum)
+}
+
+sommaArrow(7, 7);
