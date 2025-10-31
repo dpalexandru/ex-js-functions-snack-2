af41c3de6cc4192c218b72d7bdb8cc8babfe8b47
dpalexandru <alexpoian91@gmail.com>
2025-10-31 10:07:16 +0530

    snack5 -  set interval

--- functionTypesTest.js
+++ functionTypesTest.js
@@ -58,3 +58,16 @@ function creaTimer(tempo) {
 
 const timer3s = (creaTimer(3000))
 timer3s()
+
+//Crea una funzione stampaOgniSecondo con setInterval.
+//Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+//Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+function stampaOgniSecondo(messaggio) {
+  setInterval(() => {
+    console.log(messaggio);
+  }, 1000);
+}
+stampaOgniSecondo("sto stampando ogni secondo!");
+
