8e6491f8e17d9518f93760ec70971867209d4628
dpalexandru <alexpoian91@gmail.com>
2025-10-31 10:11:58 +0530

    snack6 - closure

--- functionTypesTest.js
+++ functionTypesTest.js
@@ -69,5 +69,20 @@ function stampaOgniSecondo(messaggio) {
     console.log(messaggio);
   }, 1000);
 }
-stampaOgniSecondo("sto stampando ogni secondo!");
+// stampaOgniSecondo("sto stampando ogni secondo!");
 
+
+// Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+function creaContatoreAutomatico(intervallo) {
+  let contatore = 0;
+
+  return function () {
+    setInterval(() => {
+      contatore++;
+      console.log(contatore);
+    }, intervallo);
+  };
+}
+
+const avviaContatore = creaContatoreAutomatico(1000);
+avviaContatore();
